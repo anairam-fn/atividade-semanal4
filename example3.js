694a0741202867ec2601a2cd7180be51194c4202
Mariana Nunes <marinun47@gmail.com>
2022-04-20 23:12:20 -0300

    [example3] adiciona resposta

---
+++ example3.js
@@ -0,0 +1,10 @@
+// Faça um programa que dado um número, imprime no (console.log) a tabuada do mesmo de 1 a 10.
+
+function printTables(num) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${num} x ${i} =`, i * num);
+  }
+  return `End`;
+}
+
+console.log(printTables(3));
